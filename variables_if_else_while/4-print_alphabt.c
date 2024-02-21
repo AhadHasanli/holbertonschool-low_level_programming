b581d4cd2b7dfca5964cb964e8f084a2d347710a
AhadHasanli <hesenliehed1@gmail.com>
2024-02-21 09:08:54 -0800

    dd

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char c;
 
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		if (c != 'e' && c != 'q')
 		{
@@ -20,4 +20,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-}
+}}
