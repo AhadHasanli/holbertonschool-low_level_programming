23abc7cc2261a41370518dcd8f020729ef7af0f6
AhadHasanli <hesenliehed1@gmail.com>
2024-02-21 09:17:50 -0800

    dd

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	for(int i = 0; i<10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
