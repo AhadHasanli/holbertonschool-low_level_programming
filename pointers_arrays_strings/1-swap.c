a2edb1b183bdf5311bbdbb9fe60f6a16554fe584
root <hesenliehed1@gmail.com git config --global user.name AhadHasanli>
2024-03-01 07:16:35 +0000

    dd

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,20 +1,15 @@
-#include <main.h>
-
-
-
-
-
+#include "main.h"
 
 /**
- *
- *
- *
+ * swap_int - swaps values
+ * @a: pointer a
+ * @b: pointer b
  */
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
-
-}
+}}
