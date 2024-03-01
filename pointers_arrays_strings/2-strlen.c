1a5761e0e327e625bc255b3eda3d25f457c44559
root <hesenliehed1@gmail.com git config --global user.name AhadHasanli>
2024-03-01 07:40:08 +0000

    dddd

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,18 @@
-#include<math.h>
+#include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- *
- *
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int i;
 
-	while (*s != '\0')
+	i = 0;
+
+	while (s[i] != '\0')
 	{
 		i++;
 	}
