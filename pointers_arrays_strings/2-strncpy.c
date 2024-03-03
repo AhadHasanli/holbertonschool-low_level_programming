acade7a116546d60da98924e6263220f0738c780
AhadHasanli <hesenliehed1@gmail.com>
2024-03-03 16:23:06 +0000

    done

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy used for copies
+ * @dest: destinationg
+ * @src: source 
+ * @n: number of byte to copy
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
